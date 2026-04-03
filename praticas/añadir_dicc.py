39f9d5f30909560d703b05499d04e02da962e74b
Daniel Getial <danielg.soft29@gmail.com>
2026-04-02 23:51:26 -0500

    Archivo para los datos, union de diccionarios en un archivo para manejar datos, separacion de logica y datos

---
+++ praticas/añadir_dicc.py
@@ -0,0 +1,7 @@
+from generar_cuentas import menu_total
+
+"""
+Programa para solicitar al usuario elegir los platos del menu
+Seleccionar que tipo de menu desea ver (Almuerzos o bebidas)
+Ingresar 
+"""
---
+++ praticas/datos_rest.py
@@ -0,0 +1,29 @@
+menu_almuerzos = {
+    "A1": {"nombre": "pollo campesino", "precio": 42000},
+    "A2": {"nombre": "sancocho de gallina", "precio": 38000},
+    "A3": {"nombre": "chuleton de cerdo", "precio": 45000},
+    "A4": {"nombre": "costilla san luis", "precio": 48000},
+    "A5": {"nombre": "costillon montañero", "precio": 50000},
+    "A6": {"nombre": "sancocho trifasico", "precio": 52000},
+    "A7": {"nombre": "tilapia a la parrilla", "precio": 40000},
+    "A9": {"nombre": "tilapia frita", "precio": 38000},
+    "A10": {"nombre": "salmon", "precio": 60000}
+}
+
+menu_bebidas = {
+    "B1": {"nombre": "limonada natural", "precio": 8000},
+    "B2": {"nombre": "limonada de coco", "precio": 10000},
+    "B3": {"nombre": "limonada de kiwi", "precio": 11000},
+    "B4": {"nombre": "limonada cerezada", "precio": 10000},
+    "B5": {"nombre": "jarra de mandarina", "precio": 15000},
+    "B6": {"nombre": "jarra de coco", "precio": 18000},
+    "B7": {"nombre": "jarra de jugo natural", "precio": 14000},
+    "B8": {"nombre": "gaseosa personal", "precio": 5000},
+    "B9": {"nombre": "gaseosa 1.5", "precio": 8000},
+    "B10": {"nombre": "botella aguardiente", "precio": 90000}
+}
+
+menu_total = {
+    "menu_almuerzos": menu_almuerzos,
+    "menu_bebidas": menu_bebidas
+}
