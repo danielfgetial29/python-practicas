b1c3ac96d249a13bc766378df1bfc6831466fec3
Daniel Getial <danielg.soft29@gmail.com>
2026-08-28 13:44:27 -0500

    Archivo herencia.py para manejar los tipos de herencia y conceptos

---
+++ POO/herencia.py
@@ -0,0 +1,35 @@
+import os
+os.system("cls")
+
+## HERENCIA
+
+# Clase padre
+class Persona:
+    def __init__(self, nombre, edad, nacionalidad):
+        self.nombre = nombre
+        self.edad = edad
+        self.nacionalidad = nacionalidad
+
+    def hablar(self):
+        print("Hola estoy hablandoooo")
+
+class Empleado(Persona): 
+    def __init__(self, nombre, edad, nacionalidad, trabajo, salario): # dos parametros propios de esta clase hija
+        super().__init__(nombre, edad, nacionalidad) # parametros heredados de la clase padre
+        self.trabajo = trabajo
+        self.salario = salario
+
+daniel = Empleado("Daniel", 25, "Colombiano", "Programador", 800000000)
+
+daniel.hablar()
+
+
+# class Padre:
+#     def __init__(self, atributoPadre1, atributoPadre2):
+#         self.nombre_atributo1 = atributoPadre1
+#         self.nombre_atributo2 = atributoPadre2
+
+# class Hija(Padre):
+#     def __init__(self, atributoPadre1, atributoPadre2, atributoNuevo_Hija):
+#         super().__init__(atributoPadre1, atributoPadre2)
+#         self.nomre_atributo_hija = atributoNuevo_Hija
