307851acb852c71bfdf63c2291f6c196e74890ef
Daniel Getial <danielg.soft29@gmail.com>
2026-09-03 13:22:55 -0500

    Nuevos apuntes sobre como funciona el mro

--- POO/mro.py
+++ POO/mro.py
@@ -3,25 +3,32 @@ os.system("cls")
 
 #MRO
 
+class E():
+    pass
+
 class A():
+    # def hablar(self):
+    #     print("Saludo desde A..")
+    pass
+class B(E):
     def hablar(self):
-        print("Hola desde A...")
+        print("Saludo desde B..")
 
 class C(A):
-    def hablar(self):
-        print("Hola desde C...")
+    # def hablar(self):
+    #     print("Saludo desde C..")
+    pass
+class D(C,B):
+    pass
+    # def hablar(self):
+    #     print("Saludo desde D..")
 
-class B(C):
-    def hablar(self):
-        print("Hola desde B...")
+prueba_mro = D()
 
-class D(B):
-    def hablar(self):
-        print("Hola desde D...")
+prueba_mro.hablar()
 
-class F(D):
-    def hablar(self):
-        print("Hola desde F...")
+#.mro() Metodo que devuelve una lista
+print(D.mro())
 
-mro = F()
-print(F.mro())
+#.__mro__  Atributo que devuelve una tupla
+print(D.__mro__)
