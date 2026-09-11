378ad4bcec0f650e9f4bdd4a106d82f6ad8bd7c7
Daniel Getial <danielg.soft29@gmail.com>
2026-09-10 22:28:33 -0500

    @..setter

--- POO/encapsulamiento/property.py
+++ POO/encapsulamiento/property.py
@@ -9,8 +9,14 @@ class Clase():
 
     @property
     def mi_atributo(self):
+        # El acceso se realiza a través de este "método" y
+        # podría contener código extra y no un simple retorn
         return self.__mi_atributo
-    
+
+    @mi_atributo.setter
+    def mi_nuevo_atributo(self, new_atributo):
+        self.__mi_atributo = new_atributo
+        return self.mi_atributo
 # try:
 #     mi_clase = Clase("Hola atributo")
 #     print(mi_clase.mi_atributo()) # llamo a .mi_atributo como si fuera un metodo
@@ -18,7 +24,13 @@ class Clase():
 #     print(f"Error de tipo {e}")
 
 try:
+    # usando el getter *@property se lee directamente sin ()
     mi_clase = Clase("Hola atributo")
-    print(mi_clase.mi_atributo)
+    print(mi_clase.mi_atributo) 
+    
+    # usando setter cambiamos asignamos el valor directamente como una varaible
+    mi_clase.mi_nuevo_atributo = "Hola nuevo atributo"
+    print(mi_clase.mi_nuevo_atributo)
+    
 except TypeError as e:
     print(f"Error de tipo {e}")
