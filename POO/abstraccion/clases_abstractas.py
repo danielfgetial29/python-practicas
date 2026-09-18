67efccd7f2b3ad4df385830af097b79c480246d2
Daniel Getial <danielg.soft29@gmail.com>
2026-09-18 01:36:59 -0500

    Nuevo ejemplo en clases_abstractas.py viendo otra clase abstracta con su metodo concreto

--- POO/abstraccion/clases_abstractas.py
+++ POO/abstraccion/clases_abstractas.py
@@ -12,6 +12,7 @@ class Saludar(ABC):
         pass # debe ser pass ya que no haremos ninguna accion aqui
 
 # clase que hereda la clase abstracta
+# clases hijas de Saludar()
 class English(Saludar):
 
     def saludar_usuario(self):
@@ -26,6 +27,8 @@ class French(Saludar):
     def saludar_usuario(self):
         return "Salut comment vas-tu"
 
+
+## salidas de la primera clase Saludar
 saludo_en_ingles = English()
 print(saludo_en_ingles.saludar_usuario())
 
@@ -34,3 +37,24 @@ print(saludar_en_frances.saludar_usuario())
 
 saludar_en_espanol = Spanish()
 print(saludar_en_espanol.saludar_usuario())
+
+# salidas de la segunda clase Animal
+
+# otro ejemplo de clase abstracta
+class Animal(ABC):
+    @abstractmethod
+    def sonido(self):
+        pass
+
+    #metodo concreto
+    def mover(self):
+        return "El animal se esta moviendo"
+
+    #clases hijas de Animal()
+class Perro(Animal):
+    def sonido(self):
+        return "Ladrar"
+mi_perro = Perro()
+
+print(mi_perro.sonido())# uso el metodo abstacto pero adaptado a mi nueva clase
+print(mi_perro.mover())# uso el metodo concreto que ya trae la clase Padre Abstracta
